807aced672f7005c8e571f12139aceff0a08d940
lienbacher <lienbacher@icloud.com>
2022-12-04 17:13:29 +0000

    add switch to init py

--- esphome/components/netsgprotocol/__init__.py
+++ esphome/components/netsgprotocol/__init__.py
@@ -1,6 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import uart
+from esphome.components import switch
 from esphome.const import CONF_ID
 from esphome import automation
 from esphome.automation import maybe_simple_id
@@ -12,7 +13,7 @@ AUTO_LOAD = ["sensor"]
 
 netsgprotocol_ns = cg.esphome_ns.namespace("netsgprotocol")
 NetSGProtocolComponent = netsgprotocol_ns.class_(
-    "NetSGProtocolComponent", cg.PollingComponent, uart.UARTDevice
+    "NetSGProtocolComponent", cg.PollingComponent, uart.UARTDevice, switch.Switch
 )
 NetSGProtocolRestart = netsgprotocol_ns.class_(
     "NetSGProtocolRestart", automation.Action
@@ -21,6 +22,7 @@ CONF_NetSGProtocol_ID = "netsgprotocol_id"
 CONF_INVERTER_DEVICE_ID = "inverter_device_id"
 CONF_POLL_INTERVAL = "poll_interval"
 CONF_POWER_GRADE = "power_grade"
+CONF_ON_OFF_SWITCH = "on_off_switch"
 
 CONFIG_SCHEMA = cv.All(
     cv.Schema(
@@ -29,6 +31,9 @@ CONFIG_SCHEMA = cv.All(
             cv.Required(CONF_INVERTER_DEVICE_ID): cv.int_range(min=1, max=4294967295),
             cv.Optional(CONF_POLL_INTERVAL, default=1): cv.int_range(min=1, max=3600),
             cv.Optional(CONF_POWER_GRADE, default=100): cv.int_range(min=1, max=100),
+            cv.Optional(CONF_ON_OFF_SWITCH): cv.ensure_list(
+                switch.switch_schema(switch.Switch)
+            ),
         }
     ).extend(uart.UART_DEVICE_SCHEMA)
 )
@@ -45,6 +50,7 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await uart.register_uart_device(var, config)
+    await switch.new_switch(config)
     cg.add(var.set_inverter_device_id(config[CONF_INVERTER_DEVICE_ID]))
     cg.add(var.set_poll_interval(config[CONF_POLL_INTERVAL]))
     cg.add(var.set_power_grade(config[CONF_POWER_GRADE]))
